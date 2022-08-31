f0b20174fd1202b802b64b7d547d99a2a5a4b2e5
Alexis Suarez <alexissuarez024@gmail.com>
2022-08-31 14:34:17 -0300

    Hicimos que el personaje se moviera segun sea indicado, y limitamos sus movimientos para que no salga del canvas.

--- game.js
+++ game.js
@@ -1,7 +1,17 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
+
 let canvasSize;
 let elementsSize;
+
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+}
 
 window.addEventListener("load", setCanvasSize); //Aca decimos que apenas habra la ventana y termine de cargar (load) comience con la funciôn
 window.addEventListener("resize", setCanvasSize);
@@ -15,14 +25,14 @@ function setCanvasSize(){
 canvas.setAttribute("width", canvasSize);  //Aplicamos los cambios al canvas 
 canvas.setAttribute("height", canvasSize); //Aplicamos los cambios al canvas
 
-   elementsSize = canvasSize / 10;
+   elementsSize = (canvasSize / 10) - 1;
 
    startGame();
 }
 
 function startGame(){
 
-    game.font = (elementsSize - 12) + 'px Verdana';
+    game.font = (elementsSize - 10) + 'px Verdana';
     game.textAlign = 'end';
     
     const map = maps[0];
@@ -30,21 +40,84 @@ function startGame(){
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows, mapRowCols});
 
+    game.clearRect(0,0,canvasSize,canvasSize);
     mapRowCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
             const posX = elementsSize * (colI + 1);
             const posY = elementsSize * (rowI + 1);
+            
+            if (col == 'O'){
+               if (!playerPosition.x && !playerPosition.y){
+                    playerPosition.x = posX;
+                    playerPosition.y = posY;
+                    console.log({playerPosition});
+               }
+            }
+
             game.fillText(emoji, posX, posY);
         });
     });
 
+    movePlayer();
+}
 
-    // for (let row = 1; row <= 10; row++) {
-    //     for (let col = 1; col <= 10; col++) {
-    //         game.fillText(emojis[mapRowCols[row -1][col - 1]], elementsSize * col, elementsSize * row); 
-    //     }   
-    // }
-   
+function movePlayer() {
+    game.fillText(emojis['PLAYER'], playerPosition.x , playerPosition.y);
+}
+
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveByKeys(event) {
+    if (event.key == 'ArrowUp') {
+        moveUp();
+    } else if (event.key == 'ArrowLeft'){
+        moveLeft();
+    } else if (event.key == 'ArrowRight'){
+        moveRight();
+    } else if (event.key == 'ArrowDown'){
+        moveDown();
+    }
+}
+
+function moveUp() {
+    console.log('Me quiero mover para arriba');
+    if ((playerPosition.y - elementsSize) < elementsSize) {
+        console.log('No puedes avanzar');
+    } else{
+        playerPosition.y -= elementsSize;
+        startGame();
+    }
+}
+function moveLeft() {
+    console.log('Me quiero mover para la izquierda');
+    if ((playerPosition.x - elementsSize) < elementsSize) {
+        console.log('No puedes avanzar');
+    } else{
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
+}
+function moveRight() {
+    console.log('Me quiero mover para la derecha');
+    if ((playerPosition.x + elementsSize) > canvasSize) {
+        console.log('No puedes avanzar');
+    } else{
+        playerPosition.x += elementsSize;
+        startGame();
+    }
+}
+function moveDown() {
+    console.log('Me quiero mover para abajo');
+    if ((playerPosition.y + elementsSize) > canvasSize) {
+        console.log('No puedes avanzar');
+    } else{
+        playerPosition.y += elementsSize;
+        startGame();
+    }
 }
 
